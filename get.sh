47ec1fadc4c85a3ee7dec169c563b997d5229f4a
inkhey <mail@inkey-art.net>
2014-06-25 20:54:37 +0200

    Delete get.sh
    Not needed anymore

--- get.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Mini-script to Download live English wikicode
-# based on transvoyage code
-TITLE=$1
-WIKICODE="temp"
-wget --quiet -O $WIKICODE "https://en.wikivoyage.org/w/index.php?title=$TITLE&action=raw"
